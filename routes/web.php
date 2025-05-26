c9b4c9ddee18599ccf8d7efc646037fd7500e894
Estiven9900 <nicolassoriano1404@gmail.com>
2025-05-26 13:20:09 -0500

    Se agregaron rutas para el módulo de cultivos en la API y la interfaz de usuario

--- routes/web.php
+++ routes/web.php
@@ -1,17 +1,29 @@
 <?php
 
 use Illuminate\Support\Facades\Route;
+use App\Http\Controllers\CultivoController;
 
+// Página de bienvenida
 Route::get('/', function () {
     return view('welcome');
 });
 
+// Rutas protegidas con autenticación y verificación de email
 Route::middleware([
     'auth:sanctum',
     config('jetstream.auth_session'),
     'verified',
 ])->group(function () {
+
+    // Ruta del dashboard
     Route::get('/dashboard', function () {
         return view('dashboard');
     })->name('dashboard');
+
+    // Rutas para el módulo de cultivos
+    Route::get('/cultivos/formcultivo', [CultivoController::class, 'create'])->name('cultivos.create');
+    Route::post('/cultivos', [CultivoController::class, 'store'])->name('cultivos.store');
+    Route::get('/index', [CultivoController::class, 'index'])->name('cultivos.index');
+    Route::put('/cultivos/{id}', [CultivoController::class, 'update'])->name('cultivos.update');
+    Route::delete('/cultivos/{id}', [CultivoController::class, 'destroy'])->name('cultivos.destroy');
 });
