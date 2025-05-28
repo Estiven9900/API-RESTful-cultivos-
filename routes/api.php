7e949f49faaf9c07b09c3b2372df5881f3fa6239
Estiven9900 <nicolassoriano1404@gmail.com>
2025-05-28 15:35:02 -0500

    Se agregó la ruta de inicio de sesión en la API para el controlador de autenticación.

--- routes/api.php
+++ routes/api.php
@@ -2,12 +2,12 @@
 
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
+use App\Http\Controllers\Api\AuthController;
 use App\Http\Controllers\Api\CultivoApiController;
 
 Route::get('/user', function (Request $request) {
     return $request->user();
 })->middleware('auth:sanctum');
 
-
-
+Route::post('login', [AuthController::class, 'login']);
 Route::apiResource('cultivos', CultivoApiController::class);
